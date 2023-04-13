b626443b45cb8b51a9649ba64855437fb8fd3077
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-04-14 01:54:23 +0300

    new design added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles/index.scss"
 import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Header />
     </div>
   );
 }
