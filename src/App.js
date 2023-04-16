8a12d8b052527ab81b335fd4c6f27868a7b29025
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-04-16 22:01:58 +0300

    changes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./styles/index.scss"
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
+import Discover from "./Components/Discover";
+import Search from "./Components/Search";
+import Favourites from "./Components/Favourites";
+import Playlist from "./Components/Playlist";
+import Charts from "./Components/Charts"; 
 
 function App() {
   return (
