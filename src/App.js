6d41012cd5e0f149d8bc25ea8738666305ca1e8d
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-04-16 22:08:21 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import Search from "./Components/Search";
 import Favourites from "./Components/Favourites";
 import Playlist from "./Components/Playlist";
 import Charts from "./Components/Charts"; 
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
      <Navbar />
      <Header />
+     <Routes>
+      <Route path="/Discover" element={<Discover />} />
+      <Route path="/Search" element={<Search />} />
+      <Route path="/Favourites" element={<Favourites />} />
+      <Route path="/Playlist" element={<Playlist />} />
+      <Route path="/Charts" element={<Charts />} />
+     </Routes>
     </div>
   );
 }
