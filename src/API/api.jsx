a36c8b04b9d808bf096beab89f4b1188d8a893ee
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-04-29 11:15:28 +0300

    working with API

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -1,26 +1,7 @@
-import axios from "axios";
-
-const options = {
-  method: 'GET',
-  url: 'https://spotify23.p.rapidapi.com/search/',
-  params: {
-    q: '<REQUIRED>',
-    type: 'multi',
-    offset: '0',
-    limit: '10',
-    numberOfTopResults: '5'
-  },
-  headers: {
-    'X-RapidAPI-Key': '4af006ccc7mshc0f072f880e50b5p16e687jsne19cf5dabfe8',
-    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-  }
-};
 
 
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+
+
+
 
--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useState, useEffect } from 'react';
+
 import "./styles/index.scss"
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
@@ -9,8 +12,40 @@ import Charts from "./Components/Charts";
 import { Routes, Route } from "react-router-dom";
 
 
+
 function App() {
+  const [albums, setAlbums] = useState();
  
+  const options = {
+    method: 'GET',
+    url: 'https://spotify23.p.rapidapi.com/search/',
+    params: {
+      q: '<REQUIRED>',
+      type: 'multi',
+      offset: '0',
+      limit: '10',
+      numberOfTopResults: '5'
+    },
+    headers: {
+      'X-RapidAPI-Key': '4af006ccc7mshc0f072f880e50b5p16e687jsne19cf5dabfe8',
+      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+  };
+  
+  
+  
+  useState(() => {
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      setAlbums(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+  }, [])
+    
+    console.log(albums);
+  
 
    
   return (
